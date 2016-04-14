17348785460c4b3c837801a25c39f5f25b8fe036
Richard Hattersley <rhattersley@gmail.com>
2016-04-14 13:09:54 +0100

    Add plotting timings

---
+++ SELFE.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ncks --no-coords --output=SELFE.nc -v ele,x,y,depth http://comt.sura.org/thredds/dodsC/data/comt_1_archive/inundation_tropical/VIMS_SELFE/Hurricane_Ike_3D_final_run_with_waves
